7c4191fa97cfd1b409cc515b1cb162c347d1234d
Cole Haus <colehaus@cryptolab.net>
2019-08-17 21:25:17 -0700

    Avoid "boolean blindness" with GDP

--- src/Data/Graph/Causal.purs
+++ src/Data/Graph/Causal.purs
@@ -11,26 +11,39 @@ import Data.Graph.Causal.Utility (after, before, distinctTwoSets)
 import Data.List.NonEmpty (NonEmptyList)
 import Data.List.NonEmpty as NEL
 import Data.Map as Map
-import Data.Maybe (maybe)
+import Data.Maybe (Maybe(..), isJust, maybe)
 import Data.Set (Set)
 import Data.Set as Set
 import Data.TwoSet (TwoSet(..))
 import GDP.Named (Named, name2, name3, unName)
-import GDP.Proof (Proof)
+import GDP.Proof (Proof, axiom)
 
-collider ::
+data IsCollider collider path graph
+
+isCollider ::
   forall p g k v.
   Ord k =>
-  Proof (IsPathOf p g) -> k -> Named p (NonEmptyList k) -> Named g (Graph k v) -> Boolean
-collider _ k path graph =
-  Foldable.any (\a -> Graph.isParentOf (unName graph) a k) (after k (unName path)) &&
-  Foldable.any (\b -> Graph.isParentOf (unName graph) b k) (before k (unName path))
+  Proof (IsPathOf p g) -> k -> Named p (NonEmptyList k) -> Named g (Graph k v) ->
+  Maybe (Proof (IsCollider k p g))
+isCollider _ k path graph =
+  if test then Just axiom else Nothing
+  where
+    test =
+      Foldable.any (\a -> Graph.isParentOf (unName graph) a k) (after k (unName path)) &&
+      Foldable.any (\b -> Graph.isParentOf (unName graph) b k) (before k (unName path))
+
+data AreDConnected vertices conditionedOn graph
 
 areDConnected ::
-  forall n m g k v. Ord k =>
+  forall n m g k v.
+  Ord k =>
   Proof (AreDisjoint n m) ->
-  Named n (TwoSet k) -> Named m (Set k) -> Named g (Graph k v) -> Boolean
-areDConnected p ks conditionedOn g = not <<< Set.isEmpty $ dConnectedBy p ks conditionedOn g
+  Named n (TwoSet k) -> Named m (Set k) -> Named g (Graph k v) ->
+  Maybe (Proof (AreDConnected n m g))
+areDConnected p ks conditionedOn g =
+  if test then Just axiom else Nothing
+  where
+    test = not <<< Set.isEmpty $ dConnectedBy p ks conditionedOn g
 
 dConnectedBy ::
   forall n m g k v.
@@ -43,7 +56,10 @@ dConnectedBy _ ks conditionedOn g =
   where
     unblocked path = nonCollidersDisjointFromW && collidersAncestorsOfW
       where
-        { yes, no } = NEL.partition (\k -> collider (pathOf_isPathOf path) k path g) (unName path)
+        { yes, no } = 
+          NEL.partition 
+            (\k -> isJust $ isCollider (pathOf_isPathOf path) k path g) 
+            (unName path)
         nonCollidersDisjointFromW =
           Set.isEmpty (Set.fromFoldable no `Set.intersection` unName conditionedOn)
         collidersAncestorsOfW =
@@ -63,16 +79,21 @@ dConnectedTo _ k conditionedOn g =
   where
     isDConnectedToK v =
       name2 (MkTwoSet (unName k) v) g (\kv g' ->
-        maybe false (\p -> areDConnected p kv conditionedOn g') $
+        maybe false (\p -> isJust $ areDConnected p kv conditionedOn g') $
         disjointnessTwoSet kv conditionedOn)
+
+data AreDSeparated vertices conditionedOn graph
 
 areDSeparated ::
   forall n m g k v.
   Ord k =>
   Proof (AreDisjoint n m) ->
-  Named n (TwoSet k) -> Named m (Set k) -> Named g (Graph k v) -> Boolean
+  Named n (TwoSet k) -> Named m (Set k) -> Named g (Graph k v) ->
+  Maybe (Proof (AreDSeparated n m g))
 areDSeparated p ks conditionedOn g =
-  Set.isEmpty $ dConnectedBy p ks conditionedOn g
+  if test then Just axiom else Nothing
+  where
+    test = Set.isEmpty $ dConnectedBy p ks conditionedOn g
 
 dSeparatedFrom ::
   forall n m k v.
@@ -84,7 +105,7 @@ dSeparatedFrom _ k conditionedOn g =
    where
      isDSeparatedFromK v =
        name2 (MkTwoSet (unName k) v) g (\kv g' ->
-         maybe false (\p -> areDSeparated p kv conditionedOn g') $
+         maybe false (\p -> isJust $ areDSeparated p kv conditionedOn g') $
          disjointnessTwoSet kv conditionedOn)
 
 dSeparations :: forall k v. Ord k => Set k -> Graph k v -> Set (TwoSet k)
@@ -95,7 +116,7 @@ dSeparations conditionedOn' g =
   where
     areDSeparated' ks' =
       name3 ks' conditionedOn' g (\ks conditionedOn g' ->
-        maybe false (\p -> areDSeparated p ks conditionedOn g') $
+        maybe false (\p -> isJust $ areDSeparated p ks conditionedOn g') $
         disjointnessTwoSet ks conditionedOn)
 
 instruments ::
@@ -115,27 +136,34 @@ instruments p causeEffect conditionedOn g =
     connected =
       dConnectedTo (cause_disjoint p) (cause causeEffect) conditionedOn g
 
+data IsInstrument instrument causeEffect conditionedOn graph
+
 isInstrument ::
-  forall n m k v.
+  forall i n m g k v.
   Ord k =>
   Proof (AreDisjoint n m) ->
-  k -> Named n { cause :: k, effect :: k } -> Named m (Set k) -> Graph k v -> Boolean
+  Named i k -> Named n { cause :: k, effect :: k } -> Named m (Set k) -> Named g (Graph k v) ->
+  Maybe (Proof (IsInstrument i n m g))
 isInstrument p i ks conditionedOn g =
-  Set.member i $ instruments p ks conditionedOn g
+  if test then Just axiom else Nothing
+  where
+    test = Set.member (unName i) $ instruments p ks conditionedOn (unName g)
 
 intervene :: forall k v. Ord k => k -> Graph k v -> Graph k v
 intervene k g = foldr removePointers g <<< Map.keys <<< Graph.toMap $ g
   where
     removePointers = Graph.alterEdges (map $ Set.filter (_ /= k))
 
+data SatBackdoor causeEffect conditionedOn graph
+
 satisfyBackdoor ::
   forall n m g k v.
   Ord k =>
   Proof (AreDisjoint n m) ->
-  Named n { cause :: k, effect :: k } -> Named m (Set k) -> Named g (Graph k v) -> Boolean
+  Named n { cause :: k, effect :: k } -> Named m (Set k) -> Named g (Graph k v) ->
+  Maybe (Proof (SatBackdoor n m g))
 satisfyBackdoor _ causeEffect conditionedOn g =
-  Foldable.all (not <<< descendsFromCause) (unName conditionedOn) &&
-  Foldable.all pathBlocked pathsPointingToCause
+  if test then Just axiom else Nothing
   where
     { cause, effect } = unName causeEffect
     pathBlocked = 
@@ -145,3 +173,6 @@ satisfyBackdoor _ causeEffect conditionedOn g =
     pathsPointingToCause =
       Set.filter ((==) cause <<< NEL.last <<< unName) $
       allUndirectedPaths (MkTwoSet cause effect) g
+    test =
+      Foldable.all (not <<< descendsFromCause) (unName conditionedOn) &&
+      Foldable.all pathBlocked pathsPointingToCause
